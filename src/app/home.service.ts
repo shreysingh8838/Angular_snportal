ea7f41ce2e7c216c17ddf49d02043de003aeeb04
Shrey Singh <49588412+shreysingh8838@users.noreply.github.com>
2023-05-31 11:57:59 +0530

    Created Home Page with Jsonobject api

---
+++ src/app/home.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  private apiUrl = 'http://localhost:8080/home';
+  constructor(private http: HttpClient) { }
+
+  getHomeDataList(): Observable<any> {
+    return this.http.get(this.apiUrl);
+    }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `
+  <marquee style="font-size: 30px;" >Anjali Meri jaan</marquee>
+  <div class="container-wrapper">
+    <div class="container-row">
+      <h2 class="job-title">Result</h2>
+      <div class="link-list" id="jobs">
+      <a *ngFor="let result of resultSet" href= {{result.link}}>{{ result.title }}</a>
+      </div>
+    </div>
+    <div class="container-row">
+      <h2 class="job-title">Admit Card</h2>
+      <div class="link-list" id="jobs">
+      <a *ngFor="let admitCard of admitCardSet" href= {{admitCard.link}}>{{ admitCard.title }}</a>
+      </div>
+    </div>
+    <div class="container-row">
+      <h2 class="job-title">Latest Jobs</h2>
+      <div class="link-list" id="jobs">
+      <a *ngFor="let latestjob of latestJobSet" href={{latestjob.link}}>{{ latestjob.title }}</a>
+      </div>
+    </div>
+  </div>
+  `,
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  jobs: string[] = [];
+  homeData: any[] = [];
+  resultSet: any[] = [];
+  admitCardSet: any[] = [];
+  latestJobSet: any[] = [];
 
+  constructor(private homeService: HomeService) { }
+
+  ngOnInit() {
+    this.homeService.getHomeDataList().subscribe((homeData: any[]) => {
+      console.log(homeData);
+      this.homeData = homeData;
+      this.resultSet = homeData[0];
+      this.admitCardSet = homeData[1];
+      this.latestJobSet = homeData[2];
+    });
+  }
 }
+
