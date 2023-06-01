987d53b8e038072bfb8ef299d79dac20a1e9807a
Shrey Singh <49588412+shreysingh8838@users.noreply.github.com>
2023-06-01 07:27:54 +0530

    Minor tweaks in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeService } from '../app/home.service';
 
 @Component({
   selector: 'app-root',
-  template: `<header>
-              <h1 class="header-text">Sarkari Naukri Portal</h1>
-            </header>
-            <nav>
-              <a routerLink="home">Home</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'resultSet'}">Result</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'latestjob'}">Latest Jobs</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'admitcard'}">Admit Cards</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'answerkey'}">Answer Keys</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'admission'}">Admission Forms</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'syllabus'}">Syllabus</a>
-              <a routerLink="singlelist" [queryParams]="{set: 'syllabus'}">Contact Us</a>
-            </nav>
-            <router-outlet></router-outlet>`,
+  template: `
+            <!DOCTYPE html>
+            <html>
+              <head>
+                  <title>Sarkari Naukri Portal</title>
+              </head>
+              <body>
+                <header>
+                  <h1 class="header-text">Sarkari Naukri Portal</h1>
+                </header>
+                <nav>
+                  <a routerLink="home">Home</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'resultSet'}">Result</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'latestjob'}">Latest Jobs</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'admitcard'}">Admit Cards</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'answerkey'}">Answer Keys</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'admission'}">Admission Forms</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'syllabus'}">Syllabus</a>
+                  <a routerLink="singlelist" [queryParams]="{set: 'syllabus'}">Contact Us</a>
+                </nav>
+                <router-outlet></router-outlet>
+              </body>
+            </html>
+
+            `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sarkariresult';
 }
